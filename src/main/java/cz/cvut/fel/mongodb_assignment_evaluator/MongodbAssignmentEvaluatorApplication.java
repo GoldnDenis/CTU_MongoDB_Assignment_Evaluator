eb2fca0eeba4308f8d74d926333666a85a911ca3
Pak, Denis <pakdenis@fel.cvut.cz>
2024-11-14 13:49:44 +0100

    :tada: Updated spring version

---
+++ src/main/java/cz/cvut/fel/mongodb_assignment_evaluator/MongodbAssignmentEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.mongodb_assignment_evaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbAssignmentEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbAssignmentEvaluatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/cvut/fel/mongodb_assignment_evaluator/MongodbAssignmentEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.mongodb_assignment_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbAssignmentEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
